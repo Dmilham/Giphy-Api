f181d9c0546b49bddd13965637aa5cdcdb80c503
Daniel Milham <Dmilham462@gmail.com>
2016-11-28 22:49:55 -0500

    Update logic.js

--- assets/javascipt/logic.js
+++ assets/javascipt/logic.js
@@ -32,7 +32,7 @@ $("#pause").css('display', 'block');
 	function displayTopicInfo(){
 		
 		var topic = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC";
 
 		
 		
